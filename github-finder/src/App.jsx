91738e82b0126d2a64360f7cb5a13a3c3da1e491
dominikdeimel <d.deimel@field-interactive.com>
2019-06-13 11:56:10 +0200

    refactroed to functional components and destructering

--- github-finder/src/App.jsx
+++ github-finder/src/App.jsx
@@ -1,17 +1,19 @@
 import React, { Component, Fragment } from 'react';
 import './App.css';
-import Navbar from './components/layout/Navbar'
-import Users from './components/users/Users'
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
 
 class App extends Component {
     render () {
         return (
             <Fragment>
                 <Navbar
-                    title={"Gihub Finder"}
-                    icon={"fab fa-github"}
+                    title={'Gihub Finder'}
+                    icon={'fab fa-github'}
                 />
-                <Users/>
+                <div className="container">
+                    <Users/>
+                </div>
             </Fragment>
         );
     }
--- github-finder/src/components/users/UserItem.jsx
+++ github-finder/src/components/users/UserItem.jsx
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class UserItem extends Component {
-    render () {
-        const {avatar_url, html_url, login} = this.props.user;
-        return (
-            <div className={'card text-center'}>
-                <img src={avatar_url} className={'round-img'} alt="" style={{width: '60px'}}/>
-                <h3>{login}</h3>
-                <div>
-                    <a target="_blank" href={html_url} className="btn btn-dark btn-sm my-1">More</a>
-                </div>
+const UserItem = ({user: {avatar_url, html_url, login}}) => {
+    return (
+        <div className={'card text-center'}>
+            <img src={avatar_url} className={'round-img'} alt="" style={{width: '60px'}}/>
+            <h3>{login}</h3>
+            <div>
+                <a target="_blank" rel="noopener noreferrer" href={html_url}
+                   className="btn btn-dark btn-sm my-1">More</a>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export default UserItem;
